deecf414202451459701584f7e27cfc245dfc9f7
Olem93 <ole.overdal@gmail.com>
2026-08-27 16:00:57 +0200

    V1.5
    WIP MVC. Nye lyder.
    Kan nå trykke på døren når lys er på, og trykke på Shrek i backgrunnen når lyset er av.

--- script.js
+++ script.js
@@ -1,18 +1,80 @@
+//model
+
+const app = document.getElementById("app");
+const audioAmbience = new Audio("soundtrack-lamp.mp3");
 let autoChange = 1;
+//let shrekChange = 1;
 let timeOut;
 let seconds = 5000;
 let isLightOff = false;
+//let isDoorShrekOff = false;
 let audioFlicker = new Audio("lampflicker.mp3");
 let audioBuzz = new Audio("lampsound.mp3");
 let audioSwamp = new Audio("swampDialogue.mp3");
 let audioStartling = new Audio("startlingSound.mp3");
 let AudionotButtons = new Audio("notTheButtons.mp3");
 let audioMuffinman = new Audio("muffinMan.mp3");
+let audioLightswitch = new Audio("lightswitch.mp3");
+let audioSteps1 = new Audio("steps1.mp3");
+let audioSteps2 = new Audio("steps2.mp3");
+let audioAllstar = new Audio("allstar.mp3");
 
-// function lightsOff() {
-//   //document.body.style.backgroundImage = "url('lyspærePåBG.jpg')";
-//   document.body.style.backgroundColor = "red";
-// }
+//view
+
+pageView();
+function pageView() {
+  app.innerHTML = /*html*/ `
+    <div style="background-color: transparent; width: 32px; height: 45px; position: absolute; top: 440px; left: 200px;" 
+    onclick="buttonClicked(); new Audio('lightswitch.mp3').play();"></div>
+    <div style="background-color: transparent; width: 140px; height: 600px; position: absolute; top: 120px; left: 800px;" 
+    onclick="doorClicked();"></div>
+     <div style="background-color: transparent; width: 140px; height: 300px; position: absolute; top: 240px; left: 600px;" 
+    onclick="shrekClicked();"></div>
+    <img style="width: 400px; height: auto; position: absolute; top: 730px; left: 500px;" src="soundtrack-enable.png"
+    onclick="playAmbience();">
+    `;
+}
+
+//controller
+
+//---shrekdoor//
+
+function doorClicked() {
+  if (shrekState()) {
+    document.body.style.backgroundImage = "url('lightOffBG5.jpg')";
+    stopAudio();
+    audioSteps2.play();
+    audioAllstar.play();
+    seconds = 13000;
+    stopTimeout();
+  } else {
+    changeLightState();
+  }
+}
+
+function shrekState() {
+  isLightOff = !isLightOff;
+  return !isLightOff;
+}
+
+function shrekClicked() {
+  if (changeLightState()) {
+    document.body.style.backgroundImage = "url('lightOffBG9.jpg')";
+    stopAudio();
+    audioLightswitch.play();
+    audioStartling.play();
+    audioSwamp.play();
+    seconds = 6000;
+    stopTimeout();
+  } else {
+    changeLightState();
+  }
+}
+
+//---lights//
+function playAmbience() {
+  audioAmbience.play();
+}
 
 function buttonClicked() {
   if (changeLightState()) {
@@ -38,77 +100,60 @@ function lightsOff() {
     audioBuzz.play();
     seconds = 500;
   } else if (autoChange === 2) {
-    document.body.style.backgroundImage = "url('lightOffBG2.jpg')";
-    stopAudio();
+    document.body.style.backgroundImage = "url('lightOnBG.jpg')";
+    audioBuzz.play();
     seconds = 200;
   } else if (autoChange === 3) {
     document.body.style.backgroundImage = "url('lightOnBG.jpg')";
     audioBuzz.play();
-    seconds = 500;
+    seconds = 300;
   } else if (autoChange === 4) {
-    document.body.style.backgroundImage = "url('lightOffBG3.jpg')";
-    stopAudio();
-    seconds = 200;
-  } else if (autoChange === 5) {
     document.body.style.backgroundImage = "url('lightOnBG.jpg')";
     audioBuzz.play();
     seconds = 800;
+  } else if (autoChange === 5) {
+    document.body.style.backgroundImage = "url('lightOffBG2.jpg')";
+    stopAudio();
+    seconds = 200;
   } else if (autoChange === 6) {
-    document.body.style.backgroundImage = "url('lightOffBG4.jpg')";
+    document.body.style.backgroundImage = "url('lightOffBG2.jpg')";
     stopAudio();
     seconds = 200;
   } else if (autoChange === 7) {
-    document.body.style.backgroundImage = "url('lightOnBG.jpg')";
-    audioBuzz.play();
-    seconds = 100;
-    seconds = 4000;
-  } else if (autoChange === 8) {
-    document.body.style.backgroundImage = "url('lightOffBG5.jpg')";
+    document.body.style.backgroundImage = "url('lightOffBG2.jpg')";
     stopAudio();
-    seconds = 400;
+    seconds = 500;
+  } else if (autoChange === 8) {
+    document.body.style.backgroundImage = "url('lightOffBG3.jpg')";
+    stopAudio();
+    seconds = 800;
   } else if (autoChange === 9) {
-    document.body.style.backgroundImage = "url('lightOnBG.jpg')";
-    audioBuzz.play();
-    seconds = 100;
-    seconds = 300;
+    document.body.style.backgroundImage = "url('lightOffBG4.jpg')";
+    stopAudio();
+    seconds = 800;
   } else if (autoChange === 10) {
+    document.body.style.backgroundImage = "url('lightOffBG8.jpg')";
+    audioSteps1.play();
+    stopAudio();
+    seconds = 800;
+  } else if (autoChange === 11) {
+    document.body.style.backgroundImage = "url('lightOffBG11.jpg')";
+    audioMuffinman.play();
+    stopAudio();
+    seconds = 5000;
+  } else if (autoChange === 12) {
     document.body.style.backgroundImage = "url('lightOffBG6.jpg')";
     stopAudio();
-    seconds = 300;
-  } else if (autoChange === 11) {
-    document.body.style.backgroundImage = "url('lightOnBG.jpg')";
-    audioBuzz.play();
-    seconds = 100;
-    seconds = 800;
-  } else if (autoChange === 12) {
+    seconds = 600;
+  } else if (autoChange === 13) {
     document.body.style.backgroundImage = "url('lightOffBG7.jpg')";
     stopAudio();
-    seconds = 400;
-  } else if (autoChange === 13) {
-    document.body.style.backgroundImage = "url('lightOnBG.jpg')";
-    audioBuzz.play();
-    seconds = 100;
-    seconds = 500;
+    seconds = 300;
   } else if (autoChange === 14) {
-    document.body.style.backgroundImage = "url('lightOffBG8.jpg')";
+    document.body.style.backgroundImage = "url('lightOffBG2.jpg')";
     stopAudio();
-    seconds = 200;
-  } else if (autoChange === 15) {
-    document.body.style.backgroundImage = "url('lightOnBG.jpg')";
-    audioBuzz.play();
-    seconds = 100;
-    seconds = 900;
-  } else if (autoChange === 16) {
-    document.body.style.backgroundImage = "url('lightOffBG11.jpg')";
-    stopAudio();
-    audioMuffinman.play();
-    seconds = 5000;
-  } else if (autoChange === 17) {
-    document.body.style.backgroundImage = "url('lightOffBG9.jpg')";
-    stopAudio();
-    audioStartling.play();
-    audioSwamp.play();
-    seconds = 5000;
+    AudionotButtons.play();
+    seconds = 3000;
   } else {
     document.body.style.backgroundImage = "url('lightOnBG.jpg')";
     stopTimeout();
@@ -119,6 +164,7 @@ function lightsOff() {
   autoChange = getRandomNumber();
   timeOut = setTimeout(lightsOff, seconds);
 }
+
 function stopTimeout() {
   audioBuzz.pause();
   audioBuzz.currentTime = 0;
@@ -130,5 +176,5 @@ function stopAudio() {
   audioBuzz.currentTime = 0;
 }
 function getRandomNumber() {
-  return Math.floor(Math.random() * 18) + 1;
+  return Math.floor(Math.random() * 15) + 1;
 }
